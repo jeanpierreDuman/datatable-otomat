626b7b06e5eae97c4798a709e06c01f7e583edd2
Jean-Pierre DUMAN <duman.jeanpierre@gmail.com>
2020-03-15 02:14:29 +0100

    good code

--- src/Service/DatatableService.php
+++ src/Service/DatatableService.php
@@ -7,18 +7,17 @@ use Doctrine\ORM\EntityManager;
 class DatatableService {
     
     private $em;
-    private $params;
+    private $request;
     private $columns;
     private $class;
     private $where;
 
-    public function initDatatable($params, array $columns = [], $class = null, array $where = [])
+    public function initDatatable($request, array $columns = [], $class = null, array $where = [])
     {
-        $this->params = $params;
+        $this->request = $request;
         $this->columns = $columns;
         $this->class = $class;
         $this->where = $where;
-        $this->getMainData();
     }
 
     public function __construct(EntityManager $em) 
@@ -26,55 +25,59 @@ class DatatableService {
         $this->em = $em;
     }
     
-    public function getColumns() {
-        return $this->columns;
-    }
-
-    public function getConditions($where, $order, $limit, $start)
+    public function getData($where, $order, $limit, $start)
     {
-        $societies = $this->em->getRepository($this->class)->findAll();
-        $totalData = count($societies);
+        $entities = $this->em->getRepository($this->class)->findBy($where);
+        $totalData = count($entities);
         
-        $societiesFiltered = $this->em->getRepository($this->class)->findBy(
+        $entitiesFiltered = $this->em->getRepository($this->class)->findBy(
             $where, $order, $limit, $start
         );
         
-        $totalFiltered = count($societiesFiltered);
+        $totalFiltered = count($entitiesFiltered);
         
         return [
             'totalData' => $totalData,
             'totalFiltered' => $totalFiltered,
-            'data' => $societiesFiltered
+            'data' => $entitiesFiltered
         ];
     }
 
-    public function getMainData() 
+    public function getParams($value = null) 
     {
-        $orderColumn = $this->getColumns()[$this->params->get('order')[0]['column']];
-        $dirOrderColumn = $this->params->get('order')[0]['dir'];
-        $limit = $this->params->get('length');
-        $start = $this->params->get('start');
+        $orderColumn = $this->getColumns()[$this->request->get('order')[0]['column']];
+        $dirOrderColumn = $this->request->get('order')[0]['dir'];
+        $limit = $this->request->get('length');
+        $start = $this->request->get('start');
 
-        $data = $this->getConditions(            
+        $data = $this->getData(            
             $this->where,
             [$orderColumn => $dirOrderColumn],
             $limit,
             $start
         );
         
+        if($value !== null) {
+            return $data[$value];
+        }
+        
         return $data;
     }
     
-    public function getJson(array $array = []) {
+    public function getJsonData(array $array = []) {
 
         $json = [
-            "draw" => intval($this->params->get('draw')),
-            "recordsTotal" => intval($this->getMainData()['totalFiltered']),
-            "recordsFiltered" => intval($this->getMainData()['totalData']),
+            "draw" => intval($this->request->get('draw')),
+            "recordsTotal" => intval($this->getParams('totalFiltered')),
+            "recordsFiltered" => intval($this->getParams('totalData')),
             "data" => $array
         ];
 
         return $json;
+    }
+    
+    public function getColumns() {
+        return $this->columns;
     }
 }
 
